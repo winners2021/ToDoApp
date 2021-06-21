f07757b01b649883bf95d3c1d25c9ed46312a09f
winners@26 <teshiwhite@yahoo.com>
2021-06-21 18:03:46 +0100

    initial commit

---
+++ app/src/main/java/com/tetsoma/todoapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tetsoma.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    var username :"String"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
